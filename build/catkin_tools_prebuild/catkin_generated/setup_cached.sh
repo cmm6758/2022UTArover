c68a56fc38f8aeb7b2e4410e3fc2003a30c19a41
Ubuntu <ubuntu@ubuntu.lan>
2022-11-15 18:13:15 +0000

    first commit

---
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROS_DISTRO='noetic'
+export ROS_ETC_DIR='/opt/ros/noetic/etc/ros'
+export ROS_MASTER_URI='http://localhost:11311'
+export ROS_PACKAGE_PATH='/home/ubuntu/catkin_ws/build/catkin_tools_prebuild:/opt/ros/noetic/share'
+export ROS_PYTHON_VERSION='3'
+export ROS_ROOT='/opt/ros/noetic/share/ros'
+export ROS_VERSION='1'
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/catkin_ws/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export PWD='/home/ubuntu/catkin_ws/build/catkin_tools_prebuild'
